b0c07e0a222b483acc3196cf311826cdefc9a511
FulmineGiallo <39560628+FulmineGiallo@users.noreply.github.com>
2024-03-07 11:46:21 +0100

    Aggiunti Commenti

--- app/src/main/java/com/example/melogiri/util/BevandeCallback.java
+++ app/src/main/java/com/example/melogiri/util/BevandeCallback.java
@@ -3,7 +3,7 @@ package com.example.melogiri.util;
 import com.example.melogiri.model.Bevanda;
 
 import java.util.List;
-
+/* Interfaccia che consente di attendere la chiamata dal server */
 public interface BevandeCallback {
     void onBevandeLoaded(List<Bevanda> productList);
 }
