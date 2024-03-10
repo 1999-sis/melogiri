9242bffd79a649d0af0c59ab393e2f7a73f8ba0a
FulmineGiallo <39560628+FulmineGiallo@users.noreply.github.com>
2024-03-10 21:27:41 +0100

    -Testare il riconoscimento con il viso

--- app/src/main/java/com/example/melogiri/controller/ControllerRegister.java
+++ app/src/main/java/com/example/melogiri/controller/ControllerRegister.java
@@ -1,6 +1,8 @@
 package com.example.melogiri.controller;
 
 import android.content.Intent;
+
+import androidx.biometric.BiometricManager;
 import androidx.biometric.BiometricPrompt;
 import android.util.Log;
 import android.widget.Toast;
@@ -11,10 +13,13 @@ import com.example.melogiri.util.SocketAPI;
 import com.example.melogiri.view.LoginActivity;
 import com.example.melogiri.view.RegisterActivity;
 
+import java.util.concurrent.Executors;
+
 public class ControllerRegister {
     SocketAPI socket = new SocketAPI("209.38.244.243", 8080);
 
-    public void register(RegisterActivity registerActivity, String nome, String cognome, String data, String email, String password) {
+    public void register(RegisterActivity registerActivity, String nome, String cognome, String data, String email, String password)
+    {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -45,28 +50,52 @@ public class ControllerRegister {
         });
         thread.start();
     }
-
-    public void showBiometricPrompt(LoginActivity loginActivity) {
+    public void showBiometricPrompt(LoginActivity loginActivity)
+    {
+        BiometricManager biometricManager = BiometricManager.from(loginActivity);
+        switch (biometricManager.canAuthenticate()) {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                Log.d("LOGIN_FACE", "Biometric authentication supported");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Log.e("LOGIN_FACE", "No biometric features available on this device.");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Log.e("LOGIN_FACE", "Biometric features are currently unavailable.");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                Log.e("LOGIN_FACE", "The user hasn't enrolled any biometrics.");
+                break;
+        }
         BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
-                .setTitle("Biometric Authentication")
-                .setDescription("Please authenticate with your biometrics to continue")
-                .setDeviceCredentialAllowed(true)
+                .setTitle("Login with your face")
+                .setSubtitle("Please use your biometric credentials to log in")
+                .setConfirmationRequired(true)
+                .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_STRONG)
                 .build();
 
         BiometricPrompt biometricPrompt = new BiometricPrompt(loginActivity,
-                ContextCompat.getMainExecutor(loginActivity),
-                new BiometricPrompt.AuthenticationCallback() {
-                    @Override
-                    public void onAuthenticationSucceeded(BiometricPrompt.AuthenticationResult result) {
-                        super.onAuthenticationSucceeded(result);
-                    }
+                Executors.newSingleThreadExecutor(), new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationSucceeded(BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                // L'autenticazione è riuscita, puoi procedere con il login.
+            }
 
-                    @Override
-                    public void onAuthenticationFailed() {
-                        super.onAuthenticationFailed();
-                    }
-                });
+            @Override
+            public void onAuthenticationError(int errorCode, CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                // Si è verificato un errore durante l'autenticazione.
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                // L'autenticazione non è riuscita.
+            }
+        });
 
         biometricPrompt.authenticate(promptInfo);
+
     }
 }
